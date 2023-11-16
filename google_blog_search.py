2c027a04650f15e3d4c4dd44dff3441f79cbc2d6
HOTTA Tatsuya <mi.ki.ri.dev@gmail.com>
2023-11-16 16:54:47 +0900

    最初の5件

--- google_blog_search.py
+++ google_blog_search.py
@@ -8,7 +8,7 @@ client = openai.Client()
 
 urls = search("RPGツクール", tld="jp", lang="ja", stop=255)
 
-for url in urls:
+for url in urls[0:5]:
     print(url)
     got = requests.get(url)
 
@@ -23,11 +23,11 @@ for url in urls:
         messages=[
             {"role": "system", "content": "あなたは渡されたテキストが個人（会社でも）ブログかどうかを判断するAIです。"},
             {"role": "system", "content": "判断した結果は、0.0~1.0の間の数値で表してください。"},
+            {"role": "system", "content": "{like_blog_or_not: 0.5}"},
             {"role": "user", "content": bodytext[0:1920]},
         ],
         model="gpt-4",
         max_tokens=2048,
+        response_format={"type": "json_object"},
     )
     print(completion.choices[0].message)
-    # とりあえず1回だけ
-    break
